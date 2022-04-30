ec47f833ca4b32c193b3c87f0941c34665863ef3
Mohamedlee27 <leonk8070@gmail.com>
2022-04-30 20:28:12 +0300

    added a class and funtions in user.py

---
+++ user.py
@@ -0,0 +1,34 @@
+from pydoc import doc
+
+
+class User: 
+    '''
+    class that generates new instances of users
+    '''
+
+    user_list = []
+
+    def __init__(self,username,password): 
+        '''
+        The __init__ method helps to define properties for our objects.The __init__ function is called every time an object is created from a class.
+        '''
+        
+        self.username = username
+        self.password = password
+    
+    def save_user(self):
+        '''
+        Save_user method saves the user object into the user list.
+        '''
+
+
+        User.user_list.append(self)
+
+    def delete_user(self):
+         '''
+        delete_user method deletes the user object from the user list.
+        '''
+
+
+
+         User.user_list.remove(self)

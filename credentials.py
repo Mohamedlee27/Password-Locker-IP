74560ab8a22c83b8333276fac717757359a7c7cd
Mohamedlee27 <leonk8070@gmail.com>
2022-04-30 20:45:23 +0300

    added check account and save functions

--- credentials.py
+++ credentials.py
@@ -11,3 +11,21 @@ class Credentials:
         self.account = account
         self.username = username
         self.password = password
+
+    def save_credentials(self):
+        '''Saves the  credentials in the  credentials list.'''
+        Credentials.credentials_list.append(self)
+        
+    # def delete_credentials(self):
+    #     Credentials.credentials_list.remove(self)
+
+    # @classmethod
+    # def display_credentials(cls, account):
+    #     return cls.credentials_list
+
+    @classmethod
+    def check_account(cls, account, username, password):
+        for credential in cls.credentials_list:
+            if credential.account == account and credential.username == username and credential.password == password:
+                return True
+        return False 

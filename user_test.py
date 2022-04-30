410d99843896de3b569e516c0931f6360a8728f4
Mohamedlee27 <leonk8070@gmail.com>
2022-04-30 20:40:37 +0300

    added more functions

--- user_test.py
+++ user_test.py
@@ -23,6 +23,24 @@ class testUser(unittest.TestCase):
 
         self.assertEqual(self.new_user.username,'Lee')
         self.assertEqual(self.new_user.password,'Flash123')
+     def test_save_user(self):
+
+
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1) 
+
+
+     def test_save_several_users(self):
+
+
+
+
+        self.new_user.save_user()
+        test_user = User('username','password')
+        test_user.save_user()
+        self.assertEqual(len(User.user_list), 2)
+
 
 
      if __name__ == '__main__':

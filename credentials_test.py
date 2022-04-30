34fb4398d1cf8ca735e99fb9c01242964cf9e8ab
Mohamedlee27 <leonk8070@gmail.com>
2022-04-30 20:52:31 +0300

    added more major functions from credentials.py

--- credentials_test.py
+++ credentials_test.py
@@ -26,5 +26,43 @@ class testCredentials(unittest.TestCase):
       self.assertEqual(self.new_credentials.username,'Lee')
       self.assertEqual(self.new_credentials.password,'Flash123')
 
-      if __name__ == '__main__':
-         unittest.main()
+    def test_save_credentials(self):
+
+       ''' tests if the Save credentials method works properly'''
+
+
+       self.new_credentials.save_credentials()
+       self.assertEqual(len(Credentials.credentials_list), 1)
+
+    # def test_delete_credentials(self):
+
+    #  '''Test delete credentials to delete class credentials'''
+
+
+    #  self.new_credentials.save_credentials()
+    #  test_credentials = Credentials('account', 'username','password')
+    #  test_credentials.save_credentials()
+
+
+    # def test_display_all_credentials(self):
+    #  '''Method that returns a list of credentials'''
+    #  self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+    def test_login(self):
+
+        self.new_credentials.save_credentials()
+        login = Credentials('Lee', 'Flash123')
+        login.save_credentials()
+        check_account = Credentials.check_account('Lee', 'Flash123')
+        self.assertTrue(check_account)
+
+
+
+
+
+
+        def test_gen_password(self):
+
+         ''' tests to generate password automatically'''
+
+        self.new_credentials.gen_password()
+        self.assertEqual(self.new_credentials.password, 'Flash123')
